8883cbaa55a0575408f2df0c43b7900c0fd3680c
Santiago Macías <santiagomacias8@hotmail.com>
2022-01-14 17:09:10 -0500

    Fixing the first command

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 # Crypto Stats
+
 ![GitHub](https://img.shields.io/github/license/sanmacorz/crypto-stats-bot)
 ![GitHub](https://img.shields.io/github/commit-activity/m/sanmacorz/crypto-stats-bot)
 
-Crypto Stats is a [Discord](https://discord.com) bot for getting cryptocurrencies market data and statistics. It's made with the [CoinGecko free API](https://www.coingecko.com/en/api), more specifically using the wrapper from the project [pycoingecko](https://github.com/man-c/pycoingecko) written in Python, also for the Discord bot integration it's using the [discord.py](https://github.com/Rapptz/discord.py) project.
+Crypto Stats is a [Discord](https://discord.com) bot for getting cryptocurrencies market data and statistics. It's made with the [CoinGecko free API](https://www.coingecko.com/en/api), more specifically using the wrapper from the project [pycoingecko](https://github.com/man-c/pycoingecko) written in Python, also for the Discord bot integration it's using the [nextcord](https://github.com/nextcord/nextcord) project.
 
 ### Getting Started
+
 ```bash
 git clone https://github.com/sanmacorz/crypto-stats-bot.git
 cd crypto-stats-bot/
 pip3 install -r requirements.txt
 ```
+
 ### Usage
 
 ```bash
--- main.py
+++ main.py
@@ -1,26 +1,50 @@
 #!/usr/bin/env python3
-import discord
-from discord.ext import commands
+import nextcord
+from nextcord.ext import commands
 import asyncio
 from pycoingecko import CoinGeckoAPI
 import matplotlib.pyplot as plt
 import functions
 
 # Set initial values
-token = functions.load_token('token.key')
+token = functions.load_token("token.key")
 prefix = "$"
 bot = commands.Bot(command_prefix=prefix)
 cg = CoinGeckoAPI()
 
-# Display the name of the specified coin 
+# Display the name of the specified coin
 @bot.command()
-async def price(ctx, arg):
+async def price(ctx, arg1, arg2):
     request = cg.get_coins_list()
-    search = str(arg)
-    print(arg)
+    search = str(arg1)
     for data in request:
-        if (data["id"] == search):
-            await ctx.send(data)
+        if data["id"] == search.lower():
+            arg1 = data["id"]
+            arg2 = arg2.lower()
+            request = cg.get_price(ids=arg1, vs_currencies=arg2)
+            await ctx.send(request)
+        elif data["symbol"] == search.lower():
+            arg1 = data["id"]
+            arg2 = arg2.lower()
+            request = cg.get_price(ids=arg1, vs_currencies=arg2)
+            elements = []
+            [elements.extend([key, value]) for key, value in request.items()]
+            [elements.extend([key, value]) for key, value in elements[1].items()]
+            del elements[1]
+            request = elements[0].capitalize() + elements[1].upper() + str(elements[2])
+            await ctx.send(request)
+        elif data["name"] == search.capitalize():
+            arg1 = data["id"]
+            arg2 = arg2.lower()
+            request = cg.get_price(ids=arg1, vs_currencies=arg2)
+            await ctx.send(request)
+
 
 # Initialize the bot by giving it the token
 bot.run(token)
+
+# TO DO:
+# Prettify the price output
+# Implement error messages for the user
+# Move commands to a single file
+# Implement Embed messages for configuration
